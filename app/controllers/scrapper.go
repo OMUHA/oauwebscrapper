a24b87d31534cea6d76411496ed47e888c65bc47
oau <ombeniaidani@gmail.com>
2023-10-06 13:41:09 +0300

    added

--- app/controllers/scrapper.go
+++ app/controllers/scrapper.go
@@ -19,9 +19,9 @@ import (
 	"time"
 )
 
-var endCount = 564211
+var endCount = 464211
 var studentsLimit = 10000
-var startCount = 364211
+var startCount = 264211
 
 func DownloadAppData(ctx *fiber.Ctx) error {
 	c := colly.NewCollector(
